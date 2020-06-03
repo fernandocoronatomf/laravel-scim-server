ab2996412bba3af672522493ea9296f72d75a990
Fernando <fernando.ferreira@whispli.com>
2020-06-03 22:45:17 +1000

    Adding binding middleware and changing configuration

--- src/RouteProvider.php
+++ src/RouteProvider.php
@@ -1,10 +1,9 @@
 <?php
+
 namespace ArieTimmerman\Laravel\SCIMServer;
 
 use Route;
-use Illuminate\Support\Facades\Auth;
 use ArieTimmerman\Laravel\SCIMServer\Exceptions\SCIMException;
-use Illuminate\Http\Request;
 
 /**
  * Helper class for the URL shortener
@@ -17,17 +16,18 @@ class RouteProvider
     {
         Route::prefix(self::$prefix)->group(function () use ($options) {
             Route::prefix('v2')->middleware([
+                'bindings',
                 'ArieTimmerman\Laravel\SCIMServer\Middleware\SCIMHeaders'
             ])
                 ->group(function () use ($options) {
                     self::allRoutes($options);
                 });
-            
+
             $routeWrongVersion = function () {
                 throw (new SCIMException('Only SCIM v2 is supported. Accessible under ' . url(self::$prefix . '/v2')))->setCode(501)
                     ->setScimType('invalidVers');
             };
-            
+
             Route::get('v1', '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@wrongVersion');
             Route::prefix('v1')->group(function () use ($options, $routeWrongVersion) {
                 Route::fallback('\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@wrongVersion');
@@ -49,10 +49,10 @@ class RouteProvider
     public static function publicRoutes(array $options = [])
     {
         Route::get("/scim/v2/ServiceProviderConfig", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ServiceProviderController@index')->name('scim.serviceproviderconfig');
-        
+
         Route::get("/scim/v2/Schemas", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\SchemaController@index');
         Route::get("/scim/v2/Schemas/{id}", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\SchemaController@show')->name('scim.schemas');
-        
+
         Route::get("/scim/v2/ResourceTypes", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceTypesController@index');
         Route::get("/scim/v2/ResourceTypes/{id}", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceTypesController@show')->name('scim.resourcetype');
     }
@@ -60,17 +60,17 @@ class RouteProvider
     private static function allRoutes(array $options = [])
     {
         Route::post('.search', '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@notImplemented');
-        
+
         // TODO: Use the attributes parameters ?attributes=userName, excludedAttributes=asdg,asdg (respect "returned" settings "always")
         Route::get('/{resourceType}/{resourceObject}', '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@show')->name('scim.resource');
         Route::get("/{resourceType}", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@index')->name('scim.resources');
-        
+
         Route::post("/{resourceType}", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@create');
-        
+
         Route::put("/{resourceType}/{resourceObject}", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@replace');
         Route::patch("/{resourceType}/{resourceObject}", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@update');
         Route::delete("/{resourceType}/{resourceObject}", '\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@delete');
-        
+
         Route::fallback('\ArieTimmerman\Laravel\SCIMServer\Http\Controllers\ResourceController@notImplemented');
     }
 }
--- src/SCIMConfig.php
+++ src/SCIMConfig.php
@@ -2,9 +2,8 @@
 
 namespace ArieTimmerman\Laravel\SCIMServer;
 
-use ArieTimmerman\Laravel\SCIMServer\SCIM\Schema;
-use ArieTimmerman\Laravel\SCIMServer\Helper;
 use ArieTimmerman\Laravel\SCIMServer\Attribute\AttributeMapping;
+use ArieTimmerman\Laravel\SCIMServer\SCIM\Schema;
 
 class SCIMConfig
 {
@@ -21,12 +20,12 @@ class SCIMConfig
     public function getUserConfig()
     {
         return [
-                
+
             // Set to 'null' to make use of auth.providers.users.model (App\User::class)
             'class' => Helper::getAuthUserClass(),
-            
+
             'validations' => [
-    
+
                 'urn:ietf:params:scim:schemas:core:2.0:User:userName' => 'required',
                 'urn:ietf:params:scim:schemas:core:2.0:User:password' => 'nullable',
                 'urn:ietf:params:scim:schemas:core:2.0:User:active' => 'boolean',
@@ -34,131 +33,86 @@ class SCIMConfig
                 'urn:ietf:params:scim:schemas:core:2.0:User:emails.*.value' => 'required|email',
                 'urn:ietf:params:scim:schemas:core:2.0:User:roles' => 'nullable|array',
                 'urn:ietf:params:scim:schemas:core:2.0:User:roles.*.value' => 'required',
-    
+
             ],
-    
+
             'singular' => 'User',
             'schema' => [Schema::SCHEMA_USER],
-    
+
             //eager loading
             'withRelations' => [],
             'map_unmapped' => true,
             'unmapped_namespace' => 'urn:ietf:params:scim:schemas:laravel:unmapped',
             'description' => 'User Account',
-            
+
             // Map a SCIM attribute to an attribute of the object.
             'mapping' => [
-                
+
                 'id' => AttributeMapping::eloquent("id")->disableWrite(),
-                
+
                 'externalId' => null,
-                
+
                 'meta' => [
                     'created' => AttributeMapping::eloquent("created_at")->disableWrite(),
                     'lastModified' => AttributeMapping::eloquent("updated_at")->disableWrite(),
-                    
+
                     'location' => (new AttributeMapping())->setRead(function ($object) {
                         return route('scim.resource', [
                             'name' => 'Users',
                             'id' => $object->id
                         ]);
                     })->disableWrite(),
-                    
+
                     'resourceType' => AttributeMapping::constant("User")
                 ],
-                
+
                 'schemas' => AttributeMapping::constant([
                     'urn:ietf:params:scim:schemas:core:2.0:User',
                     'example:name:space',
                 ])->ignoreWrite(),
-                
+
                 'example:name:space' => [
                     'cityPrefix' => AttributeMapping::eloquent('cityPrefix')
                 ],
-                
+
                 'urn:ietf:params:scim:schemas:core:2.0:User' => [
-                    
-                    'userName' => AttributeMapping::eloquent("name"),
-                    
+
+                    'userName' => null,
+
                     'name' => [
-                        'formatted' => AttributeMapping::eloquent("name"),
-                        'familyName' => null,
-                        'givenName' => null,
+                        'formatted' => AttributeMapping::eloquent("full_name"),
+                        'familyName' => AttributeMapping::eloquent("last_name"),
+                        'givenName' => AttributeMapping::eloquent("first_name"),
                         'middleName' => null,
                         'honorificPrefix' => null,
                         'honorificSuffix' => null
                     ],
-                    
+
                     'displayName' => null,
                     'nickName' => null,
                     'profileUrl' => null,
-                    'title' => null,
+                    'title' => AttributeMapping::eloquent("job_title"),
                     'userType' => null,
                     'preferredLanguage' => null, // Section 5.3.5 of [RFC7231]
                     'locale' => null, // see RFC5646
-                    'timezone' => null, // see RFC6557
-                    'active' => null,
-                    
+                    'timezone' => AttributeMapping::eloquent("timezone"), // see RFC6557
+                    'active' => AttributeMapping::eloquent("user_active"),
+
                     'password' => AttributeMapping::eloquent('password')->disableRead(),
-                    
+
                     // Multi-Valued Attributes
                     'emails' => [[
-                            "value" => AttributeMapping::eloquent("email"),
-                            "display" => null,
-                            "type" => AttributeMapping::constant("other")->ignoreWrite(),
-                            "primary" => AttributeMapping::constant(true)->ignoreWrite()
-                    ],[
-                            "value" => AttributeMapping::eloquent("email"),
-                            "display" => null,
-                            "type" => AttributeMapping::constant("work")->ignoreWrite(),
-                            "primary" => AttributeMapping::constant(true)->ignoreWrite()
-                    ]],
-                    
-                    'phoneNumbers' => [[
-                        "value" => null,
+                        "value" => AttributeMapping::eloquent("email"),
                         "display" => null,
-                        "type" => null,
-                        "primary" => null
+                        "type" => AttributeMapping::constant("work")->ignoreWrite(),
+                        "primary" => AttributeMapping::constant(true)->ignoreWrite()
                     ]],
-                    
-                    'ims' => [[
-                        "value" => null,
-                        "display" => null,
-                        "type" => null,
-                        "primary" => null
-                    ]], // Instant messaging addresses for the User
-                    
-                    'photos' => [[
-                        "value" => null,
-                        "display" => null,
-                        "type" => null,
-                        "primary" => null
-                    ]],
-                    
-                    'addresses' => [[
-                        'formatted' => null,
-                        'streetAddress' => null,
-                        'locality' => null,
-                        'region' => null,
-                        'postalCode' => null,
-                        'country' => null
-                    ]],
-                    
-                    'groups' => [[
-                        'value' => null,
-                        '$ref' => null,
-                        'display' => null,
-                        'type' => null,
-                        'type' => null
-                    ]],
-                    
-                    'entitlements' => null,
-                    'roles' => null,
+                    'roles' => AttributeMapping::eloquent('roles'),
                     'x509Certificates' => null
                 ],
-                            
+
             ]
-            ];
+        ];
     }
 
     public function getConfig()
@@ -168,6 +122,6 @@ class SCIMConfig
             'Users' => $this->getUserConfig()
         ]
 
-        ;
+            ;
     }
 }
